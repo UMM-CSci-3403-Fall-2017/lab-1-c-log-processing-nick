0707c325bc83c5c9f93bca85e3b5d7ece14b6bdb
bushw011 <bushw011@morris.umn.edu>
2017-09-06 16:53:59 -0500

    We worked on process_client_logs.sh. It is incomplete as of now, but we are using a recursive grep call to gather all the relevant lines, and then we are piping that to an awk command which will select the appropriate columns. The issue now is to deal with valid and invalid user lines, as we will have to select different columns for the corresponding lines. And after that we have to only allow hours to appear in the time columns. That part should be solved simply by piping to another awk command where we will use the -F tag and select ":" as a field. That way we'll be able to eliminate the time fields we don't care about.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,8 +2,11 @@
 
 Directory=$1
 
-cd $Directory
-
-cat * | 
+#cd $Directory
 
 
+##awk 'match([A-Z][a-z]+(?= [0-9])|[0-9]+(?= [0-9])|[0-9]+(?=:[0-9]+:[0-9]+)|(?<=user) [A-z]+|(?<=password for) [A-z]+|(?<=password for invalid user) [A-z]+/g|[0-9]+\.[0-9]\.[0-9]+\.[0-9]+ ' test4scrubs.txt > somethingstuff.txt
+
+##grep -r "Failed password" $Directory | cat > somethingstuff.txt
+
+grep -r "Failed password" $Directory | awk '{if ($9 == "invalid") print $11, $13 ' failed_login_data.txt
