86598c99798719c946e4f553df1d0ecd82309d18
holma198 <holma198@morris.umn.edu>
2017-09-07 11:51:40 -0500

    Add a slightly modified version of wrap_contents.sh to work within create_username_dist.sh in order to add header and footers from the html_components folders and concatinate it with the data processed in create_username_dist.sh

---
+++ bin/wrap_contents.sh
@@ -0,0 +1,14 @@
+
+#!/usr/bin/bash
+
+##Name of file whose contents exist in the <p> html tag
+contentsFile=$1
+##name of the header and string (root name) to be concatinated to the contentsFile
+desiredHeaderFoot=$2
+##name of concatinated file that will be the resulting html file.
+resultFile=$3
+
+Directory=$4
+
+##This line concatinates the header contents and footer into a single html file 
+cat "${desiredHeaderFoot}_header.html" "$contentsFile" "${desiredHeaderFoot}_footer.html" > $Directory/"$resultFile"
