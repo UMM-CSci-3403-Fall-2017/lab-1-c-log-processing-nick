f5a05b46ca14cd06b5d8caeef692b30d2689d16d
holma198 <holma198@morris.umn.edu>
2017-09-07 11:54:57 -0500

    Add file that creates all necessary data.add rows for the users and counts of the reoccurances of those users.  This function still requires work, it needs to traverse through each sub directory and add all of those failed.login.txt files into the function

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,5 +2,18 @@
 
 Directory=$1
 
-awk '{ print $4}' failed_login_data.txt|sort -u| cat > bobTheTomato.txt
+##awk '{ print $4}' failed_login_data.txt|sort -u| cat > temp1.txt
+
+listOfSubD=find $Directory "failed_login_data.txt"
+
+echo $listOfSubD
+
+awk '{ print $4}' $Directory/failed_login_data.txt|sort | uniq -c| awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}'|cat >>  temp1.txt
+
+./bin/wrap_contents.sh temp1.txt ./html_components/username_dist username_dist.html $Directory
+
+rm temp1.txt
+
+
+
 
