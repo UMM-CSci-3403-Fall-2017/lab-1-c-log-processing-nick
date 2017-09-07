c7072ede1dfb8f2bca5ca3248f072b70210ff39f
holma198 <holma198@morris.umn.edu>
2017-09-06 21:12:55 -0500

    We succesfully used a recursive grep to gather the correct lines, which we pipe to an awk command that filters the appropriate columns, along with a substr call that filters out minutes and seconds for the time. We also handle the two different types of lines with a conditional inside of the awk call. This is then piped to a cat command that overwrites to failed_login_data.txt from where the scipt is called

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,7 +6,6 @@ Directory=$1
 
 
 ##awk 'match([A-Z][a-z]+(?= [0-9])|[0-9]+(?= [0-9])|[0-9]+(?=:[0-9]+:[0-9]+)|(?<=user) [A-z]+|(?<=password for) [A-z]+|(?<=password for invalid user) [A-z]+/g|[0-9]+\.[0-9]\.[0-9]+\.[0-9]+ ' test4scrubs.txt > somethingstuff.txt
-
 ##grep -r "Failed password" $Directory | cat > somethingstuff.txt
 
-grep -r "Failed password" $Directory | awk '{if ($9 == "invalid") print $11, $13 ' failed_login_data.txt
+grep -rh "Failed password" $Directory | awk '{if ($9 == "invalid") print $1, $2, substr($3,0,2), $11, $13; else print $1, $2, substr($3,0,2), $9, $11}'| cat > failed_login_data.txt
