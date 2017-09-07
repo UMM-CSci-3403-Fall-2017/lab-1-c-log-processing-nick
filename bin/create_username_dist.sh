6f3f04b374143aa93798732732bc6ec4735a546a
bushw011 <bushw011@morris.umn.edu>
2017-09-07 18:48:20 -0500

    Create a shell script file that successfully creates an html file with all of the failed_login_data.txt files for each respective directory, given the directory contains the file, and this group of files is formatted into javascript lines which gets wrapped inbetween the respective header and footer.  Bats tests pass.:

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -4,16 +4,22 @@ Directory=$1
 
 ##awk '{ print $4}' failed_login_data.txt|sort -u| cat > temp1.txt
 
-listOfSubD=find $Directory "failed_login_data.txt"
+#listOfSubD=find $Directory "failed_login_data.txt"
 
-echo $listOfSubD
+#echo $listOfSubD
+for f in $Directory/*; do
+	if [ -f $f/failed_login_data.txt ]; then
+	cat $f/failed_login_data.txt >> $Directory/temp2.txt
+fi
+done
 
-awk '{ print $4}' $Directory/failed_login_data.txt|sort | uniq -c| awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}'|cat >>  temp1.txt
 
+##find $Directory -name "failed_login_data.txt" | cat > plumbus.txt
+awk '{ print $4}' $Directory/temp2.txt|sort | uniq -c| awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}'|cat >>  temp1.txt
 ./bin/wrap_contents.sh temp1.txt ./html_components/username_dist username_dist.html $Directory
 
 rm temp1.txt
-
+#rm $Directory/temp2.txt
 
 
 
