beeef6ab98d85c9e97edceb79881b98467ab41ad
bushw011 <bushw011@morris.umn.edu>
2017-09-07 19:08:30 -0500

    holma198 spelled concatenate wrong

--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -3,12 +3,12 @@
 
 ##Name of file whose contents exist in the <p> html tag
 contentsFile=$1
-##name of the header and string (root name) to be concatinated to the contentsFile
+##name of the header and string (root name) to be concatenated to the contentsFile
 desiredHeaderFoot=$2
 ##name of concatinated file that will be the resulting html file.
 resultFile=$3
 
 Directory=$4
 
-##This line concatinates the header contents and footer into a single html file 
+##This line concatenates the header contents and footer into a single html file 
 cat "${desiredHeaderFoot}_header.html" "$contentsFile" "${desiredHeaderFoot}_footer.html" > $Directory/"$resultFile"
