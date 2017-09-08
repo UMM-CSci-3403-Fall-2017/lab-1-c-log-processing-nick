2d6c88ca58a7bcb17c78f62c3056d882a3e9d336
bushw011 <bushw011@morris.umn.edu>
2017-09-07 19:10:41 -0500

    Added comments through process_client_logs.sh to explain the process throughtout the script.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,10 +2,5 @@
 
 Directory=$1
 
-#cd $Directory
-
-
-##awk 'match([A-Z][a-z]+(?= [0-9])|[0-9]+(?= [0-9])|[0-9]+(?=:[0-9]+:[0-9]+)|(?<=user) [A-z]+|(?<=password for) [A-z]+|(?<=password for invalid user) [A-z]+/g|[0-9]+\.[0-9]\.[0-9]+\.[0-9]+ ' test4scrubs.txt > somethingstuff.txt
-##grep -r "Failed password" $Directory | cat > somethingstuff.txt
-
+# This makes a recursive grep call (with the h tag to avoid outputting the file paths) for any file containing the string "Failed password" inside the given Directory argument. This is then piped to an awk which uses a conditional statementfor the two possible line structures. And is given the correct columns to print given the respective line. We also use awk's substr function here so that we only print hours in the time column. This is then concatenated into the failed_login_data.txt file inside the given directory.
 grep -rh "Failed password" $Directory | awk '{if ($9 == "invalid") print $1, $2, substr($3,0,2), $11, $13; else print $1, $2, substr($3,0,2), $9, $11}'| cat > $Directory/failed_login_data.txt
