2b4ba1b4c56066add5730468d94d358f42a51e41
bushw011 <bushw011@morris.umn.edu>
2017-09-12 18:11:10 -0500

    Fix function create_country_dist.sh no successfully passes bats tests by first creating a temp file of unique country codes and associated count.  Then concatenate the produced javascript, with the country codes and count, and wraps in the header and footer of the country dist html provided in the html_components folder.

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -10,4 +10,11 @@ done
 
 awk '{print $5}' $Directory/temp.txt|sort | cat > IPs.txt
 
-join  -1 1 -2 1  IPs.txt etc/country_IP_map.txt > temp2.txt
+join  -1 1 -2 1  IPs.txt etc/country_IP_map.txt | awk '{print $2}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' | cat > temp2.txt
+
+./bin/wrap_contents.sh temp2.txt ./html_components/country_dist country_dist.html $Directory
+
+rm temp2.txt
+rm IPs.txt
+rm $Directory/temp.txt
+
