8d6eb60f84a966e0340b657ec927f28c2c459505
holma198 <holma198@morris.umn.edu>
2017-09-18 15:32:54 -0500

    Add comments and added a rm command to remove the SCRATCH directory.

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/bash
 
+## Creating a variable for all the command line arguments.
 Tarchives=( "$@" )
 
+## making the Scratch temporary directory
 mkdir SCRATCH 
 
-headDir=$PWD
-echo ${Tarchives[@]}
-echo ${Tarchives[0]}
-echo ${Tarchives[1]}
-echo $PWD
+## for loop that takes every tgz file for every argument.  In each run of the loop, the tgz file is stripped of its path and stored in variable noPath.  then a directory is made of noPath without the extension "tgz."  Then the file is extracted and placed in the SCRATCH directory.    
 for f in ${Tarchives[@]}; do
-	echo $f
 	noPath=${f##*/}
 	mkdir ./SCRATCH/${noPath%_*}
 	tar -xzf $f -C ./SCRATCH/${noPath%_*}
@@ -19,12 +16,14 @@ for f in ${Tarchives[@]}; do
 done
 
 
-
+## Calls necessary helper methods to construct the html file failed_login_summary.html
 ./bin/create_username_dist.sh ./SCRATCH
 ./bin/create_hours_dist.sh ./SCRATCH
 ./bin/create_country_dist.sh ./SCRATCH
 ./bin/assemble_report.sh ./SCRATCH
 
+## Move the html file to the head directory of the project 
 mv ./SCRATCH/failed_login_summary.html ./
 
-
+## remove SCRATCH directory and all of its contents.
+rm -rf ./SCRATCH
